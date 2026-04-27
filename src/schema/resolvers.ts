b5cf92701c97901861f2fbfd53075a5ebb1ffde1
daveKontro <dkontro@hotmail.com>
2026-04-27 07:29:19 -0400

    add some helpful notes

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -26,6 +26,8 @@ export const resolvers: Resolvers = {
     customer: (parent, { id }, { db }) => db.customers.findById(id),
   },
   Order: {
+    // NOTE resolves the customer relationship on Order... called by 
+    // GraphQL for each order when the client requests customer fields
     customer: (parent, args, { db }) => db.customers.findById(parent.customerId),
   },
   Mutation: {
@@ -40,3 +42,30 @@ export const resolvers: Resolvers = {
     deleteOrder: (parent, { id }, { db }) => db.orders.findByIdAndRemove(id),
   },
 }
+
+// NOTE:
+// GraphQL efficiently resolves only the fields
+// requested by the client, but whether it fetches
+// only those fields from the data source depends
+// entirely on how resolvers are implemented...
+// the example resolver implementation shown below
+// may avoid over fetching from a database or API
+//
+// Query: {
+//   orders: (parent, { filter }, { db }, info) => {
+//     const selections = info.fieldNodes[0].selectionSet?.selections ?? []
+//
+//     const fields = selections.reduce((acc: Record<string, number>, selection) => {
+//       if (selection.kind === 'Field') {
+//         acc[selection.name.value] = 1
+//       }
+//
+//       return acc
+//     }, {})
+//
+//     // filter = query criteria (docs, rows, etc.)
+//     // fields = projection (fields, columns, etc.)
+//     return db.orders.find(filter, fields)
+//   },
+//  ...
+// }
